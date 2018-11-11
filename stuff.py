eb07af5d0a77c4649c44650203a18fc87d4ba853
edsabi <38739707+edsabi@users.noreply.github.com>
2018-11-11 08:29:37 -0500

    Add files via upload

---
+++ stuff.py
@@ -0,0 +1,85 @@
+import requests
+import os
+import re
+import time
+import traceback
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+class Domain_Recon(object):
+
+
+    def GET_CTFR(self):
+        dr.subdomains = []
+        domain = input('Input Domain\n')
+
+        print('Querying certspotter database')
+        get_subdomains = requests.get("https://api.certspotter.com/v1/issuances?domain="+domain+'&include_subdomains=true&expand=dns_names')
+        subdomain_page = get_subdomains.text.split('"dns_names":[')
+
+        for page in subdomain_page[1:]:
+            page_list = page.split('],')[0].split(',')
+            for page2 in page_list:
+                dr.subdomains.append(page2.split('"')[1])
+
+        print(list(set(dr.subdomains)))
+
+    def shodan_api(self):
+        shodan_key = '?key=J8ksgexD5Oyif5PgCns3YU3FMDQZZARK'
+
+        for subdomain in list(set(dr.subdomains)):
+            time.sleep(1)
+            try:
+                print('Resolving '+subdomain)
+                host_resolve_shodan = requests.get('https://api.shodan.io/dns/resolve?hostnames='+subdomain+'&key=J8ksgexD5Oyif5PgCns3YU3FMDQZZARK', verify = False )
+                print('IP FOUND! '+host_resolve_shodan.text)
+
+                try:
+                    resolved_IP = host_resolve_shodan.text.split(':')[1].split('"')[1]
+                except:
+                    print('Null IP')
+                try:
+                    os.makedirs('target/'+time.strftime("%Y-%m-%d")+'/'+subdomain)
+                except:
+                    print (traceback.print_exc())
+
+                dr.shodan_results = requests.get('https://api.shodan.io/shodan/host/'+resolved_IP+'?key=J8ksgexD5Oyif5PgCns3YU3FMDQZZARK', verify = False).text
+                shodan_lines = dr.shodan_results.split(',')
+
+
+
+                if dr.shodan_results != '{"error": "No information available for that IP."}':
+                    with open('target/'+time.strftime("%Y-%m-%d")+'/'+subdomain+'/info.csv','w') as ifile:
+                        for line in shodan_lines:
+                            ifile.write(line+'\n')
+                    regex = re.compile(r'(CVE-[0-9]{4}-[0-9]{4})')
+                    vulns = regex.findall(dr.shodan_results)
+                    #input(vulns)
+                    try:
+
+                        for vuln in vulns:
+                            vuln_check = requests.get("http://cve.circl.lu/api/cve/"+vuln, verify = False)
+
+                            with open('target/'+time.strftime("%Y-%m-%d")+'/'+subdomain+'/'+vuln+'.txt','w') as ifile2:
+                                ifile2.write(vuln_check.text)
+                    except:
+                        traceback.print_exc()
+                else:
+                    print('Removing useless stuff')
+                    os.remove('target/'+time.strftime("%Y-%m-%d")+'/'+subdomain+'/info.csv')
+                    os.rmdir('target/'+time.strftime("%Y-%m-%d")+'/'+subdomain)
+                for i in os.listdir("target/"+time.strftime("%Y-%m-%d")):
+                    try:
+                        os.rmdir("target/"+time.strftime("%Y-%m-%d")+'/'+i)
+                    except:
+                        pass
+            except:
+                traceback.print_exc()
+                print('oops :(')
+                #input()
+
+if __name__ == '__main__':
+    dr = Domain_Recon()
+    dr.GET_CTFR()
+    dr.shodan_api()
